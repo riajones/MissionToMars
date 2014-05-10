47759444a4a9bc9fea3b28dc41a3032836261f0e
riajones <riajones@ucsc.edu>
2014-05-10 16:41:06 -0700

    Forgot to "add" a few scripts

---
+++ Assets/Scripts/dataCarry.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dataCarry : MonoBehaviour {
+	public int farms;
+	public int greenhouses;
+	public int icemines;
+	public int mines;
+	public int barracks;
+	public int explorebases;
+	public int mirrorsatellites;
+	public int researchcenters;
+	public  int population;
+	public int food;
+	public int water;
+
+	public int weight;
+
+	void Awake() {
+		DontDestroyOnLoad(transform.gameObject);
+	}
+}
